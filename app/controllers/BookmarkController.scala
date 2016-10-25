db9a8131aa62095022d60e304fbbd6b92f3e7b88
Erkin Unlu <eunlu@tripadvisor.com>
2016-10-25 23:04:01 +0100

    very crude add new bookmark page

--- app/controllers/BookmarkController.scala
+++ app/controllers/BookmarkController.scala
@@ -3,15 +3,29 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import models.Bookmark
-import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.{Action, Controller}
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.{Messages, I18nSupport, MessagesApi}
+import play.api.mvc.{Action, Controller, Flash}
 
 /**
   * Created by eunlu on 19/10/2016.
   */
+
+// TODO add appropriate links to details page
 @Singleton
 class BookmarkController @Inject() (val messagesApi: MessagesApi) extends Controller
   with I18nSupport  {
+
+  // TODO put some validation on this form
+  private val bookmarkForm: Form[Bookmark] = Form {
+    mapping(
+      "url" -> nonEmptyText,
+      "description" -> nonEmptyText,
+      "folderName" -> nonEmptyText
+    ) (Bookmark.unsaved)(Bookmark.unapplyShort)
+  }
+
   def list = Action { implicit request =>
     val bookmarks = Bookmark.findAll
 
@@ -25,4 +39,32 @@ class BookmarkController @Inject() (val messagesApi: MessagesApi) extends Contro
       Ok(views.html.bookmarks.details(bookmark))
     }.getOrElse(NotFound)
   }
+
+  def save = Action { implicit request =>
+    val bmForm = bookmarkForm.bindFromRequest()
+
+    bmForm.fold(
+      hasErrors = { form =>
+        Redirect(routes.BookmarkController.newBookmark()).
+          flashing(Flash(form.data) +
+            ("error" -> Messages("validation.errors")))
+      },
+
+      success = { newBookmark =>
+        Bookmark.add(newBookmark)
+        val message = Messages("bookmark.new.success", newBookmark.url)
+        Redirect(routes.BookmarkController.list()).flashing("success" -> message)
+      }
+    )
+  }
+
+  def newBookmark = Action { implicit request =>
+    val form =
+      if (request.flash.get("error").isDefined)
+        bookmarkForm.bind(request.flash.data)
+      else bookmarkForm
+
+    Ok(views.html.bookmarks.add(form))
+  }
+
 }
--- app/models/Bookmark.scala
+++ app/models/Bookmark.scala
@@ -6,7 +6,9 @@ package models
 case class Bookmark(url: String, description: String, folderName: String, id: Long)
 
 object Bookmark {
-  val bookmarks = Set (
+
+  // TODO this has to be a data repo
+  var bookmarks = Set (
     Bookmark("http://ggia.berkeley.edu/practice/meaningful_pictures", "Greater Good link", "GGIA", 1),
     Bookmark("http://ggia.berkeley.edu/practice/finding_silver_linings", "Greater Good link", "GGIA",  2),
     Bookmark("http://www.ybrikman.com/writing/2014/03/10/the-ultimate-guide-to-getting-started/", "Scala", "Scala", 3),
@@ -16,4 +18,16 @@ object Bookmark {
   def findAll = bookmarks.toList
 
   def findById(id: Long): Option[Bookmark] = bookmarks find { bm => bm.id == id}
+
+  // means it is unsaved yet, TODO fix later with Option type
+  def unsaved(url: String, description: String, folder: String) = Bookmark(url, description, folder, -1)
+
+  def unapplyShort(bookmark: Bookmark) =
+    Option((bookmark.url, bookmark.description, bookmark.folderName))
+
+  def add(bookmark: Bookmark): Unit = {
+    val maxId = bookmarks.maxBy(bm => bm.id).id
+
+    bookmarks += Bookmark(bookmark.url, bookmark.description, bookmark.folderName, maxId)
+  }
 }
