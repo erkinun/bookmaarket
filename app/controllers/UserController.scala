937b78a4b87610e7dbc48cf4f2893a41f395cfb6
Erkin Unlu <erkinun@gmail.com>
2017-01-23 23:05:11 +0000

    a very crude register page

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -9,14 +9,14 @@ import play.api.data.Forms.optional
 import play.api.data.Forms.text
 import play.api.data.Forms.mapping
 import play.api.data.Forms.email
-import play.api.i18n.MessagesApi
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
 
 /**
   * Created by eunlu on 22/01/2017.
   */
 @Singleton
-class UserController @Inject()(val messagesApi: MessagesApi) extends Controller {
+class UserController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
 
   val userForm = Form {
@@ -33,5 +33,9 @@ class UserController @Inject()(val messagesApi: MessagesApi) extends Controller 
       formWithErrors => BadRequest,
       user => NotImplemented
     )
+  }
+
+  def getRegisterPage() = Action { implicit request =>
+    Ok(views.html.users.register(userForm))
   }
 }
